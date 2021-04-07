b9bb9aaacb44dbc76b502391f27eb9e2d8925114
Hans Bala <hansbala@hansbala.com>
2021-04-06 20:02:05 -0400

    most of createUser done

--- client.py
+++ client.py
@@ -28,15 +28,51 @@ from support.keyserver import keyserver
 
 # DO NOT EDIT ABOVE THIS LINE ##################################################
 
+# Global Variables
+SALT_LEN = 16       # 16 bytes salt length
+KEY_LEN = 16        # 16 bytes is the symmetric key length
+
 class User:
-    def __init__(self) -> None:
+    def __init__(self, username, user_salt, user_pk, user_sk, user_symmetric_key, user_hmac) -> None:
         """
         Class constructor for the `User` class.
 
         You are free to add fields to the User class by changing the definition
         of this function.
         """
-        pass
+        self.username = username
+        self.user_salt = user_salt
+        self.user_pk = user_pk
+        self.user_sk = user_sk
+        self.user_symmetric_key = user_symmetric_key
+        self.user_hmac = user_hmac
+
+        # now that we have all the required info, we need to store the public key
+        # and this above generated data inside the keyserver and the dataserver
+        # create the python dictionary which will be stored on the data server
+        user_data_dict = {
+            'username': self.username,
+            'hashed_password': crypto.Hash(self.user_salt + self.password),
+            'user_sk_encrypted': crypto.SymmetricEncrypt(self.user_symmetric_key, crypto.SecureRandom(KEY_LEN), self.user_sk),
+            'user_hmac': self.user_hmac,
+        }
+        # get a memloc from the username (so that we can consistently access this location)
+        try:
+            user_data_memloc = memloc.MakeFromBytes(bytes(self.username))
+        except ValueError:
+            raise util.DropboxError("input to MakeFromBytes is not 16 bytes in length.")
+        # store the user_data_bytes in this memory location
+        try:
+            dataserver.Set(user_data_memloc, util.ObjectToBytes(user_data_dict))
+        except ValueError:
+            raise util.DropboxError("dataserver requires storage to be in bytes.")
+
+        # Store the public key in the keyserver
+        try:
+            keyserver.Set(username, user_pk)
+        except ValueError:
+            raise util.DropboxError("user already has a corresponding public key in database. Cannot create same username.")
+
 
     def upload_file(self, filename: str, data: bytes) -> None:
         """
@@ -91,8 +127,16 @@ def create_user(username: str, password: str) -> User:
     The specification for this function is at:
     http://dropbox.crewmate.academy/client-api/authentication/create-user.html
     """
-    # TODO: Implement!
-    raise util.DropboxError("Not Implemented")
+    # STEP 1: Create a salt for the user
+    user_salt = crypto.SecureRandom(SALT_LEN)
+    # STEP 2: Generate a PK/SK pair
+    user_pk, user_sk = crypto.AsymmetricKeyGen()
+    # STEP 3: Generate a symmetric key for the user
+    user_symmetric_key = crypto.PasswordKDF(password, user_salt, KEY_LEN)
+    # STEP 4: Generate an HMAC for the (PW + SALT) + SK with the <user_symmetric_key>
+    user_hmac = crypto.HMAC(user_symmetric_key, password + user_salt + user_sk)
+    
+    return User(username, user_salt, user_pk, user_sk, user_symmetric_key, user_hmac)
 
 def authenticate_user(username: str, password: str) -> User:
     """
